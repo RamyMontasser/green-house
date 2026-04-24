495d36b7b7c72b8c0a5de18c66cbe741348f8c8f
RamyMontasser <ramy.montasser123@gmail.com>
2026-04-24 22:46:35 +0300

    release

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(360, 690),
-      minTextAdapt: true,
+       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_ , child) {
         return MediaQuery(
